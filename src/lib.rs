d7a72ac30a441a2f84619ae8ac37acbe4817bfd5
Max Whitney <mwhitney@alumni.sfu.ca>
2024-02-08 18:44:39 +0800

    Note about character requirements for Bracket.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 //!
 //! This crate does not currently provide any special support for rendering the Ratatui cursor; if
 //! you want to show the cursor then the calling code needs to draw it.
+//!
+//! Note that Bracket (at least with normal fonts) is strictly extended ASCII / CP437, whereas
+//! Ratatui defaults to unicode for box drawing etc.. You will likely need to set such characters
+//! yourself.
 
 pub mod bterm;
 mod colours;
